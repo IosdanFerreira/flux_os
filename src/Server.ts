07cd5eca9044a89fe83dbefb74ba9e5a780677a1
Iosdan Ferreira <iosdanprferreira@gmail.com>
2023-09-16 00:40:13 -0300

    Adicionado o CORS

--- src/Server.ts
+++ src/Server.ts
@@ -5,6 +5,13 @@ import { routes } from './routes';
 
 const server = express();
 
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 server.use(express.json());
 server.use(routes);
 
