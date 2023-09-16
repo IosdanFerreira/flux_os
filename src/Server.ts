1840088a5b610e0302a0716c3191a40f40975836
Iosdan Ferreira <iosdanprferreira@gmail.com>
2023-09-16 00:51:37 -0300

    Liberação de todas as chamadas

--- src/Server.ts
+++ src/Server.ts
@@ -6,7 +6,7 @@ import { routes } from './routes';
 const server = express();
 
 server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
